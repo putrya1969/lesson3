7631c254d08e2f5738589e898dbab2e811d9fafe
putrya1969 <putrya1969@gmail.com>
2022-08-04 21:35:07 +0300

    last version application

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -10,6 +10,43 @@ namespace Lesson3
     {
         static void Main(string[] args)
         {
+            string str = "qwe";
+            if (str == "qaz")
+            {
+                Console.WriteLine("string equal qaz");
+            }
+            else 
+            {
+                Console.WriteLine("string not equal qaz");
+            }
+
+            switch (str)
+            {
+                case "qaz":
+                    Console.WriteLine("string equal qaz");
+                    break;
+                default:
+                    Console.WriteLine("string not equal qaz");
+                    break;
+            }
+            //int i = 1;
+            //int sum = 0;
+            //while (i <= 10 )
+            //{
+            //    sum += i;
+            //    i++;
+            //}
+            //Console.WriteLine(sum);
+            int i = 1;
+            int sum = 0;
+            do
+            {
+                sum += i;
+                i++;
+            }
+            while (i <= 10);
+            Console.WriteLine(sum);
+            Console.ReadKey();
         }
     }
 }
